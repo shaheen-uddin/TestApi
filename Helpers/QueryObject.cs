973f09c516213ccc26b74e0a53b81e365f0aa178
shaheen-uddin <shaheenelt@gmail.com>
2024-06-25 12:46:43 +0600

    Sorting done

--- Helpers/QueryObject.cs
+++ Helpers/QueryObject.cs
@@ -8,5 +8,7 @@ namespace TestApi.Helpers;
 public class QueryObject
 {
     public string? Symbol { get; set; } = null;
-    public String? CompanyName { get; set; } = null;
+    public string? CompanyName { get; set; } = null;
+    public string? SortBy { get; set; } = null;
+    public bool IsDescending { get; set; } = false;
 }
--- Repository/StockRepository.cs
+++ Repository/StockRepository.cs
@@ -54,6 +54,13 @@ public class StockRepository : IStockRepository
         {
             stocks = stocks.Where(s => s.Symbol.Contains(query.Symbol));
         }
+        if (!string.IsNullOrWhiteSpace(query.SortBy))
+        {
+            if (query.SortBy.Equals("Symbol", StringComparison.OrdinalIgnoreCase))
+            {
+                stocks = query.IsDescending ? stocks.OrderByDescending(s => s.Symbol) : stocks.OrderBy(s => s.Symbol);
+            }
+        }
         return await stocks.ToListAsync();
     }
 
