1a7e58c0742b8b905fabf7cc5ae306500c0f72fa
shaheen-uddin <shaheenelt@gmail.com>
2024-06-25 12:59:32 +0600

    Pagination done

--- Repository/StockRepository.cs
+++ Repository/StockRepository.cs
@@ -61,7 +61,8 @@ public class StockRepository : IStockRepository
                 stocks = query.IsDescending ? stocks.OrderByDescending(s => s.Symbol) : stocks.OrderBy(s => s.Symbol);
             }
         }
-        return await stocks.ToListAsync();
+        var skipNumber = (query.PageNumber - 1) * query.PageSize;
+        return await stocks.Skip(skipNumber).Take(query.PageSize).ToListAsync();
     }
 
 
